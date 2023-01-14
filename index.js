a81d3ab05fa21047b8a154c882c7d7108e8090c3
BellSohn <mcb_box@yahoo.com>
2023-01-14 12:40:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 3999;
 mongoose.connect('mongodb://localhost:27017/bookbackend',{useNewUrlParser:true})
 .then(()=>{
     console.log('conexion with mongo successfully stablished')
-    //crear el servidor
+    //create the server
     app.listen(port,()=>{
         console.log('server http://localhost:3999 working');
     });
