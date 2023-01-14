e8daf0c30baefaa37c2e599d4f5a3a8147b71c03
BellSohn <mcb_box@yahoo.com>
2023-01-14 12:39:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-//Ejecutar express
+//execute express
 var app = express();
 
-//archivos de rutas
+//files routes
 var user_routes = require('./routes/user');
 var book_routes = require('./routes/book');
 var loan_routes = require('./routes/loan');
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 //CORS
-// Configurar cabeceras y cors
+// set headers and CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
@@ -34,50 +34,5 @@ app.use('/api',loan_routes);
 const fileUpload = require('express-fileupload');
 app.use(fileUpload());
 
-//test methods
-/*
-app.get('/pruebaget',(req,res)=>{
-    return res.status(200).send({
-        nombre:'Mario',
-        surname:'Bellsola'
-    });
-});
-*/
-
-/*
-app.post('/pruebapost',(req,res)=>{
-    return res.status(200).send({
-        message:'Este es el texto del post'
-    });
-});
-
-app.post('/parametrosbody',(req,res)=>{
-    //console.log(req.body);
-    var bodyParams = req.body;
-    var name = bodyParams.name;
-    var surname = bodyParams.surname;
-    return res.status(200).send({
-        message:'peticion params',
-        name:name,
-        surname:surname
-    });
-});
-
-app.post('/valorurl',(req,res)=>{
-    var urlParams = req.query.valor
-    console.log(urlParams);
-    return res.status(200).send({
-        message:'parametros por url'
-    });
-});
-
-app.post('/valorsegmentourl/:id',(req,res)=>{
-    var valorsegmento = req.params.id;
-    console.log(valorsegmento);
-    return res.status(200).send({
-        message:'parametro por segmento'
-    });
-});
-*/
 
 module.exports = app;
